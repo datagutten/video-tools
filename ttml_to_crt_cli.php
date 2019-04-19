aff2a6a09e2bf027b8cf3c026530b6a726c9ec80
datagutten <datagutten@datagutten.net>
2019-04-19 12:39:53 +0200

    Add CLI script for TTML to SRT conversion

---
+++ ttml_to_crt_cli.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anders
+ * Date: 27.01.2019
+ * Time: 12.43
+ */
+require 'autoload.php';
+try {
+    $ttml_to_srt = new ttml_to_srt();
+    $ttml_to_srt->convert_file($argv[1]);
+}
+catch (Exception $e)
+{
+    echo $e->getMessage()."\n";
+}
