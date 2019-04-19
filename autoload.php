417dc53e70d26ea4136021adcc0e744d3393f98a
datagutten <datagutten@datagutten.net>
2019-04-19 12:17:06 +0200

    Add filters to autoload

--- autoload.php
+++ autoload.php
@@ -6,6 +6,9 @@
  * Time: 10:08
  */
 spl_autoload_register(function ($class_name) {
-    /** @noinspection PhpIncludeInspection */
-    include $class_name . '.php';
+    $valid_classes = array('ttml_to_srt', 'video', 'video_download', 'video_duration_check');
+    if(array_search($class_name, $valid_classes)!==false) {
+        /** @noinspection PhpIncludeInspection */
+        include $class_name . '.php';
+    }
 });
