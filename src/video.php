8f6a6710c4e002c7c04cc90d772112446527fea8
datagutten <datagutten@datagutten.net>
2020-08-28 19:32:23 +0200

    Remove unnecessary local variable

--- src/video.php
+++ src/video.php
@@ -28,8 +28,7 @@ class video
 		$time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $time);
 		sscanf($time, "%d:%d:%d", $hours, $minutes, $seconds);
 
-		$time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
-		return $time_seconds;
+        return $hours * 3600 + $minutes * 60 + $seconds;
 	}
 
     /**
