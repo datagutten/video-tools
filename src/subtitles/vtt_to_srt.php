9ddfce1bd461072e4d77a74813bb2d01a57212c4
datagutten <datagutten@datagutten.net>
2023-01-07 21:24:47 +0100

    Simplify conversion

--- src/subtitles/vtt_to_srt.php
+++ src/subtitles/vtt_to_srt.php
@@ -11,26 +11,11 @@ class vtt_to_srt extends srt
      * @param string $sub vtt subtitle to be converted
      * @return string converted srt subtitle
      */
-    public function convert($sub)
+    public function convert(string $sub): string
     {
-        //$sub = preg_replace('/([0-9:]+)\.([0-9]+) --> ([0-9:]+)\.([0-9]+)/', '$1,$2 --> $3,$4', $sub);
-        //$sub = str_replace("WEBVTT\n\n", '', $sub);
-        //$sub = str_replace("\n", "\r\n", $sub);
-        preg_match_all('/([0-9:]+)\.([0-9]+) --> ([0-9:]+)\.([0-9]+)\s+(.+?)(?:\s+\r?\n\d|\z)/s', $sub, $lines);
-        $sub='';
-
-        foreach ($lines[0] as $key=>$line)
-        {
-            $sub .= $key+1 ."\r\n";
-
-            if(strlen($lines[1][$key])===5)
-                $lines[1][$key] = '00:' . $lines[1][$key];
-            if(strlen($lines[3][$key])===5)
-                $lines[3][$key] = '00:' . $lines[3][$key];
-            //$sub .= sprintf("%s,%s --> %s,%s\r\n%s\r\n\r\n", $lines[1][$key], $lines[2][$key], $lines[3][$key], $lines[4][$key], $lines[5][$key]);
-            $lines[5][$key] = html_entity_decode($lines[5][$key]);
-            $this->add_line($lines[1][$key] . ',' . $lines[2][$key], $lines[3][$key] .','. $lines[4][$key], $lines[5][$key]);
-        }
+        $sub = preg_replace('/([0-9:]+)\.([0-9]+) --> ([0-9:]+)\.([0-9]+)/', '$1,$2 --> $3,$4', $sub);
+        $this->sub = str_replace("WEBVTT\r\n\r\n", '', $sub);
+        $this->sub .= "\r\n";
         return $this->sub;
     }
 
